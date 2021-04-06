b115a56186d1e766f4c3114bb011a87c0b7bdf51
Ryan-Grabham <72136273+Ryan-Grabham@users.noreply.github.com>
2021-04-06 23:03:45 +0100

    Add files via upload

---
+++ ShepherdsRest/js/script.js
@@ -0,0 +1,11 @@
+function openSide(){
+    var x = document.getElementById('side');
+
+    x.style.width= '30vw';
+}
+
+function closeSide(){
+    var x = document.getElementById('side');
+
+    x.style.width= '0vw';
+}
