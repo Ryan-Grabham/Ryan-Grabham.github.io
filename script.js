f9cc8c15425c9a92f69bd28244fb2e06d0266efd
Ryan-Grabham <72136273+Ryan-Grabham@users.noreply.github.com>
2021-04-06 16:55:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openSide(){
+    var x = document.getElementById('side');
+
+    x.style.width= '40vw';
+}
+
+function closeSide(){
+    var x = document.getElementById('side');
+
+    x.style.width= '0vw';
+}
