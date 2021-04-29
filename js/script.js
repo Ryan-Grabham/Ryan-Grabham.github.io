6b53a89f30840ef3f407453f8a5fbb32fe01f1bb
Ryan Grabham <Gra18001287@btc.ac.uk>
2021-04-29 10:37:55 +0100

    Final

--- js/script.js
+++ js/script.js
@@ -10,34 +10,4 @@ function closeSide() {
     x.style.width = '0vw';
 }
 
-function APIcall() {
-    var xhttp = new XMLHttpRequest();
-
-    var key = '4ea86496';
-    var searchValue = document.getElementById('search-text').value;
-    console.log(searchValue);
-    var url = 'https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=' + searchValue;
-
-    console.log(url);
-    xhttp.open('GET', url);
-
-    xhttp.send();
-
-    xhttp.onreadystatechange = function ()
-    {
-        if (this.readyState == 4 && this.status == 200) 
-        {
-            var data = JSON.parse(this.responseText);
-
-            console.log(data);
-            
-            document.getElementById('badge').src = data.teams[0].strTeamBadge;
-            document.getElementById('team').innerHTML = data.teams[0].strAlternate;
-            document.getElementById('nickname').innerHTML = data.teams[0].strKeywords;
-            document.getElementById('stadium').innerHTML = data.teams[0].strStadium;
-            
-            document.getElementById('search-text').value = null;
-        }
-    }
-}
 
